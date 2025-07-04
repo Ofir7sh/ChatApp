9c5aef375ebd82edeae20ec917b3a2b18418131c
Ofir7sh <ofir.sh95@gmail.com>
2025-07-05 02:24:15 +0300

    Wrote dockerfiles + docker-compose.yml

--- init_db/init_db.py
+++ init_db/init_db.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, text
 from app.database import Base
-from app.config import MASTER_DATABASE_URL, DATABASE_URL as APP_DATABASE_URL, DB_NAME as APP_DB_NAME
+from sqlalchemy import create_engine, text
+from app.core.config import MASTER_DATABASE_URL, DATABASE_URL as APP_DATABASE_URL, DB_NAME as APP_DB_NAME
 from app.models import user, chat_room, message  
 
 def create_database():
